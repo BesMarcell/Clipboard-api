fde3bc16849a7e5155303993f3424b3045003c00
Snapko Mikalai <kolyan@kolyan.instinctools.ru>
2016-12-05 14:10:01 +0300

    mongoose v4

--- src/db.js
+++ src/db.js
@@ -2,7 +2,4 @@ import mongoose from 'mongoose';
 import Promise from 'bluebird';
 import { config } from 'clipbeard';
 
-export default () => {
-	const db = mongoose.connect(config.get('db:mongo:connectionString'));
-	return db;
-};
+export default const db = mongoose.connect(config.get('db:mongo:connectionString'));
