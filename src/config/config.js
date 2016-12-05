5a2b151373baddfffe2293d8d8ac2a7f8c07652d
Snapko Mikalai <kolyan@kolyan.instinctools.ru>
2016-12-05 10:30:52 +0300

    add passport&mongoose

---
+++ src/config/config.js
@@ -0,0 +1,7 @@
+// Invoke 'strict' JavaScript mode
+'use strict';
+
+module.exports = {
+	db: 'mongodb://localhost/clipbeard-development',
+	sessionSecret: 'developmentSessionSecret'
+};
--- src/index.js
+++ src/index.js
@@ -1,18 +1,40 @@
 import Koa from 'koa';
 import koaRouter from 'koa-router';
 import { config, logger } from 'clipbeard';
+import sleep from './sleep';
+
+var mongoose = require('./config/mongoose');
+
+//var passport = require('./config/passport');
+import passport from 'passport';
+require('./config/passport')(passport);
 
 const app = new Koa();
 const router = koaRouter();
 
-router.get('/', async ctx => {
+var db = mongoose();
+//var passport = passport();
 
-  ctx.body = 'Hello';
+router.get('/', async ctx => {
+  ctx.body = 'Hello All together';
+  console.log('a');
+  await sleep(2000);
+  console.log('b');
+  //console.log(JSON.stringify(ctx));
+
+});
+
+router.get('/world', async ctx => {
+  ctx.body = 'Who are you?';
 });
 
 
 app.use(router.routes());
 const port = config.get('PORT') || config.get('server:port');
+
+app.use(passport.initialize());
+app.use(passport.session());
+
 app.listen(port, async () => {
 
   logger.info('Listening port %d', port);
