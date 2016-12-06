a69d2d3cc51b5fb151046cdfa556cd41b2761b77
Snapko Mikalai <kolyan@kolyan.instinctools.ru>
2016-12-06 10:56:12 +0300

    change promise to async

--- src/utils/auth.js
+++ src/utils/auth.js
@@ -7,25 +7,24 @@ passport.serializeUser((user, done) => {
   done(null, user._id);
 });
 
-passport.deserializeUser((id, done) => {
-  (async () => {
-    try {
-      const user = await User.findById(id);
-      done(null, user);
-    } catch (err) {
-      done(err);
-    }
-  })();
+passport.deserializeUser(async (id, done) => {
+  try {
+    const user = await User.findById(id);
+    done(null, user);
+  } catch (err) {
+    done(err);
+  }
 });
 
-passport.use(new local.Strategy((email, password, done) => {
-  User.findOne({email: email})
-    .then(user => {
-      if (email === user.email && password === user.password) {
-        done(null, user);
-      } else {
-        done(null, false);
-      }
-    })
-    .catch(err => done(err));
+passport.use(new local.Strategy(async (email, password, done) => {
+  try {
+    const user = await User.findOne({email: email});
+    if (email === user.email && password === user.password) {
+      done(null, user);
+    } else {
+      done(null, false);
+    }
+  } catch (err) {
+    done(err);
+  }
 }));
